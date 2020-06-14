51500dcff59d927f3b66df570c847bde1da30b7c
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-06-13 23:49:36 -0400

    First working build
    Used test app from RfgTools++ in main since it makes use of RfgTools and it's submodules. Compiles and runs without issues now.

--- Project28/Project28.cpp
+++ Project28/Project28.cpp
@@ -1,7 +1,42 @@
-﻿#include <iostream>
+﻿#include "common/Typedefs.h"
+#include "Common/timing/Timer.h"
+#include "Common/filesystem/Path.h"
+#include "Common/filesystem/File.h"
+#include "RfgTools++/formats/packfiles/Packfile3.h"
+#include <iostream>
 
 int main()
 {
+    string inputPath = "G:/RFG Unpack/data/misc.vpp_pc";
+    string outputPath = "G:/RFG Unpack 2/CppToolOutput/Unpack/";
 
-	return 0;
+    std::cout << "inputPath: \"" << inputPath << "\"\n";
+    std::cout << "outputPath: \"" << outputPath << "\"\n";
+
+    u32 numRuns = 1; //Should use higher number for more precise results. Need speed for testing though
+    std::vector<u64> times;
+    times.reserve(numRuns);
+
+    printf("Extracting packfile %d times...\n", numRuns);
+    Timer timer(true);
+
+    for (u32 i = 0; i < numRuns; i++)
+    {
+        Packfile3 packfile(inputPath);
+        packfile.ReadMetadata();
+        packfile.ExtractSubfiles(outputPath + Path::GetFileNameNoExtension(inputPath) + "/");
+        //auto weaponsXtbl = packfile.ExtractSingleFile("weapons.xtbl");
+        //File::WriteToFile("G:\\RFG Unpack\\boop.xml", weaponsXtbl.value());
+        //delete[] weaponsXtbl.value().data();
+        times.push_back(timer.ElapsedMicroseconds());
+        timer.Reset();
+    }
+
+    f32 sum = 0.0f;
+    for (auto& val : times)
+        sum += (f32)val;
+
+    std::cout << "Elapsed time (average over " << numRuns << " runs): " << sum / (f32)numRuns << "us | " << sum / (f32)numRuns / 1000.0f << "ms | " << sum / (f32)numRuns / 1000000.0f << "s\n";
+
+    return 0;
 }
