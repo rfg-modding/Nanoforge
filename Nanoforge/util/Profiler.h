d74838d5c6a516aa8c37aa6b90148be7910ce208
moneyl <8206401+Moneyl@users.noreply.github.com>
2021-09-11 01:13:06 -0400

    Profiler changes
    Simplified macros used when the profiler is disabled. Previously had a do while loop that runs once (nop macro), which should be optimized out by the compiler. Now it's just blank. Not sure why I thought that was necessary. Might be based on issues I encountered on a previous project when using tracy profiler.

--- Nanoforge/util/Profiler.h
+++ Nanoforge/util/Profiler.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define nop do {} while(0)
-
 //Define custom profiler macros so it can be disabled or switched out easily
 #ifdef TRACY_ENABLE
 #include <d3d11.h>
@@ -14,13 +12,13 @@
     #define PROFILER_SCOPED(blockName) ZoneScopedN(blockName)
     #define PROFILER_SCOPED_AUTO ZoneScoped
     #define PROFILER_FUNCTION() ZoneScoped
-    #define PROFILER_D3D11_CONTEXT(device, deviceContext) TracyD3D11Context(device, deviceContext);
-    #define PROFILER_D3D11_CONTEXT_DESTROY(ctx) TracyD3D11Destroy(ctx);
-    #define PROFILER_D3D11_ZONE(ctx, name) TracyD3D11Zone(ctx, name);
-    #define PROFILER_D3D11_COLLECT(ctx) TracyD3D11Collect(ctx);
+    #define PROFILER_D3D11_CONTEXT(device, deviceContext) TracyD3D11Context(device, deviceContext)
+    #define PROFILER_D3D11_CONTEXT_DESTROY(ctx) TracyD3D11Destroy(ctx)
+    #define PROFILER_D3D11_ZONE(ctx, name) TracyD3D11Zone(ctx, name)
+    #define PROFILER_D3D11_COLLECT(ctx) TracyD3D11Collect(ctx)
     #define PROFILER_FRAME_MARK() FrameMark
-    #define PROFILER_FRAME_MARK_START(name) FrameMarkStart(name);
-    #define PROFILER_FRAME_MARK_END(name) FrameMarkEnd(name);
+    #define PROFILER_FRAME_MARK_START(name) FrameMarkStart(name)
+    #define PROFILER_FRAME_MARK_END(name) FrameMarkEnd(name)
     #define PROFILER_SET_THREAD_NAME(threadName) tracy::SetThreadName(threadName)
     #define PROFILER_BLOCK(blockName) ZoneNamedN(#blockName, blockName, true)
     #define PROFILER_BLOCK_WRAP(blockName, code) \
@@ -30,22 +28,22 @@
     } \
 
 #else
-    #define PROFILER_EVENT(eventName) nop;
-    #define PROFILER_SCOPED(blockName) nop;
-    #define PROFILER_SCOPED_AUTO(blockName) nop;
-    #define PROFILER_FUNCTION() nop;
-    #define PROFILER_D3D11_CONTEXT(device, deviceContext) nop;
-    #define PROFILER_D3D11_CONTEXT_DESTROY(ctx) nop;
-    #define PROFILER_D3D11_ZONE(name) nop;
-    #define PROFILER_D3D11_COLLECT(ctx) nop;
-    #define PROFILER_FRAME_MARK() nop;
-    #define PROFILER_FRAME_MARK_START(name) nop;
-    #define PROFILER_FRAME_MARK_END(name) nop;
-    #define PROFILER_SET_THREAD_NAME(threadName) nop;
-    #define PROFILER_BLOCK(blockName) nop;
+    #define PROFILER_EVENT(eventName)
+    #define PROFILER_SCOPED(blockName)
+    #define PROFILER_SCOPED_AUTO(blockName)
+    #define PROFILER_FUNCTION()
+    #define PROFILER_D3D11_CONTEXT(device, deviceContext)
+    #define PROFILER_D3D11_CONTEXT_DESTROY(ctx)
+    #define PROFILER_D3D11_ZONE(name)
+    #define PROFILER_D3D11_COLLECT(ctx)
+    #define PROFILER_FRAME_MARK()
+    #define PROFILER_FRAME_MARK_START(name)
+    #define PROFILER_FRAME_MARK_END(name)
+    #define PROFILER_SET_THREAD_NAME(threadName)
+    #define PROFILER_BLOCK(blockName)
     #define PROFILER_BLOCK_WRAP(blockName, code) \
     { \
         code \
-    }; \
+    } \
 
 #endif
