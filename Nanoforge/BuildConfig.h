355b9cdd4e311b481b7374d61def8e2945f08e08
moneyl <8206401+Moneyl@users.noreply.github.com>
2022-05-01 13:44:52 -0400

    Define specific major, minor, and patch versions
    Will make it easier to compare the exe version to the version of the latest release on github in the future. Don't have to parse the local version string. Just use the versions directly.

--- Nanoforge/BuildConfig.h
+++ Nanoforge/BuildConfig.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 
+#define STRINGIFY(value) #value
+#define MAKE_VERSION_STRING(major, minor, patch, suffix) "v" STRINGIFY(major) "." STRINGIFY(minor) "." STRINGIFY(patch) suffix
+
 namespace BuildConfig
 {
     //Path to the asset folder. Varies depending on build type. See src/CMakeLists.txt.
@@ -10,5 +13,10 @@ namespace BuildConfig
     static const std::string FontAwesomePath = AssetFolderPath + "fonts/fa-solid-900.ttf";
     static const std::string VsCodeIconsPath = AssetFolderPath + "fonts/codicon.ttf";
     static const std::string ShaderPath = AssetFolderPath + "shaders/";
-    static const std::string Version = "v0.20.0-pre1";
+
+#define NanoforgeVersionMajor 0
+#define NanoforgeVersionMinor 20
+#define NanoforgeVersionPatch 0
+#define NanoforgeVersionSuffix "-pre0"
+    static const std::string Version = MAKE_VERSION_STRING(NanoforgeVersionMajor, NanoforgeVersionMinor, NanoforgeVersionPatch, NanoforgeVersionSuffix);
 }
