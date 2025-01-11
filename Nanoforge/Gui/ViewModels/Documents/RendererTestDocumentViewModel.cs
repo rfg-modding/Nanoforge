595ed82ddb3e6d93a0c937725d49b3db1f24f2eb
Luke Money <8206401+Moneyl@users.noreply.github.com>
2025-01-10 22:02:27 -0500

    Dehardcode renderer test document assets

--- Nanoforge/Gui/ViewModels/Documents/RendererTestDocumentViewModel.cs
+++ Nanoforge/Gui/ViewModels/Documents/RendererTestDocumentViewModel.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
+using System.Threading;
 using System.Threading.Tasks;
 using Avalonia;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Dock.Model.Mvvm.Controls;
+using Nanoforge.FileSystem;
 using Nanoforge.Render;
 using Nanoforge.Render.Resources;
 using RFGM.Formats.Meshes;
 using RFGM.Formats.Meshes.Shared;
+using RFGM.Formats.Peg;
+using RFGM.Formats.Peg.Models;
+using Serilog;
 using Silk.NET.Vulkan;
 using PrimitiveTopology = RFGM.Formats.Meshes.Shared.PrimitiveTopology;
 
@@ -24,92 +29,39 @@ public partial class RendererTestDocumentViewModel : Document
     private List<VertexFormat> _allStaticMeshVertexFormats = new();
     private RenderObject? _skybox = null;
 
-    public RendererTestDocumentViewModel()
-    {
-        
-    }
-    
     [RelayCommand]
     public async Task SceneInit()                     
     {
-        //TODO: Replace these hardcoded paths with packfile paths and load from the game files using PackfileVFS once its ported.
-        const string texturePath = "/home/moneyl/projects/CSVulkanRenderer/CSVulkanRenderer/assets/viking_room.png";
-        //const string meshPath = "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/autoblaster/autoblaster.csmesh_pc";
-        //const string meshPath = "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/missilepod/dlc_rocket_pod.csmesh_pc";
-        //const string meshPath = "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/multi_object_backpack_thrust/thrust.csmesh_pc";
-        //const string meshPath = "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/EDF_Super_Gauss/super_gauss_rifle.csmesh_pc";
-        //const string meshPath = "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/dlcp01_items/Unpack/toolbox/amb_toolbox.csmesh_pc";
-        const string meshPath = "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/dlcp01_items/Unpack/rpg_launcher/rpg.csmesh_pc";
-
         Renderer? renderer = (Application.Current as App)!.Renderer;
         if (renderer == null)
             return;
         
         RenderContext context = renderer.Context;
-        
-        Mesh mesh = LoadRfgStaticMeshFromFile(context, meshPath);
-        Texture2D texture = LoadTextureFromFile(context, texturePath);
+
+        //TODO: Use TextureIndex + textures list in static meshes to find and load specific pegs for each mesh
+        //string texturePath = "//data/items.vpp_pc/rpg_high.str2_pc/rpg_high.cpeg_pc";
+        string texturePath = "//data/skybox.vpp_pc/rfg_skybox.csmesh_pc.str2_pc/rfg_skybox.cpeg_pc";
+        Mesh mesh = LoadRfgStaticMeshFromPackfile(context, "//data/dlcp01_items.vpp_pc/rpg_launcher.str2_pc/rpg.csmesh_pc");
+        Texture2D texture = LoadTextureFromPackfile(context, texturePath);
         Scene.CreateRenderObject("Pixlit1UvNmap", new Vector3(0.0f, 0.0f, 0.0f), Matrix4x4.Identity, mesh, texture);
         
-        Mesh mesh2 = LoadRfgStaticMeshFromFile(context, "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/multi_object_backpack_thrust/thrust.csmesh_pc");
-        Texture2D texture2 = LoadTextureFromFile(context, "/home/moneyl/projects/CSVulkanRenderer/CSVulkanRenderer/assets/bright_future.png");
+        Mesh mesh2 = LoadRfgStaticMeshFromPackfile(context, "//data/items.vpp_pc/multi_object_backpack_thrust.str2_pc/thrust.csmesh_pc");
+        Texture2D texture2 = LoadTextureFromPackfile(context, texturePath);
         Scene.CreateRenderObject("Pixlit1UvNmap", new Vector3(-2.0f, 0.0f, 0.0f), Matrix4x4.Identity, mesh2, texture2);
         
-        Mesh mesh3 = LoadRfgStaticMeshFromFile(context, "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/EDF_Super_Gauss/super_gauss_rifle.csmesh_pc");
-        Texture2D texture3 = LoadTextureFromFile(context, "/home/moneyl/projects/CSVulkanRenderer/CSVulkanRenderer/assets/bright_future.png");
+        Mesh mesh3 = LoadRfgStaticMeshFromPackfile(context, "//data/items.vpp_pc/EDF_Super_Gauss.str2_pc/super_gauss_rifle.csmesh_pc");
+        Texture2D texture3 = LoadTextureFromPackfile(context, texturePath);
         Scene.CreateRenderObject("Pixlit1UvNmap", new Vector3(2.0f, 0.0f, -1.0f), Matrix4x4.Identity, mesh3, texture3);
         
-        Mesh mesh4 = LoadRfgStaticMeshFromFile(context, "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/items/Unpack/missilepod/dlc_rocket_pod.csmesh_pc");
-        Texture2D texture4 = LoadTextureFromFile(context, "/home/moneyl/projects/CSVulkanRenderer/CSVulkanRenderer/assets/bright_future.png");
+        Mesh mesh4 = LoadRfgStaticMeshFromPackfile(context, "//data/items.vpp_pc/missilepod.str2_pc/dlc_rocket_pod.csmesh_pc");
+        Texture2D texture4 = LoadTextureFromPackfile(context, texturePath);
         Scene.CreateRenderObject("Pixlit1UvNmap", new Vector3(-2.0f, 0.0f, 2.0f), Matrix4x4.Identity, mesh4, texture4);
         
-        Mesh mesh5 = LoadRfgStaticMeshFromFile(context, "/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/skybox/Unpack/rfg_skybox.csmesh_pc/rfg_skybox.csmesh_pc");
-        Texture2D texture5 = LoadTextureFromFile(context, "/home/moneyl/projects/CSVulkanRenderer/CSVulkanRenderer/assets/bright_future.png");
+        Mesh mesh5 = LoadRfgStaticMeshFromPackfile(context, "//data/skybox.vpp_pc/rfg_skybox.csmesh_pc.str2_pc/rfg_skybox.csmesh_pc");
+        Texture2D texture5 = LoadTextureFromPackfile(context, texturePath);
         _skybox = Scene.CreateRenderObject("Pixlit1Uv", new Vector3(-2.0f, 0.0f, 2.0f), Matrix4x4.Identity, mesh5, texture5);
         _skybox.Position = Vector3.Zero;
         _skybox.Scale = new Vector3(25000.0f);
-        
-        /*
-        List<string> meshPaths = new();
-        FindFilesRecursive("/mnt/StorageC/_RFGR_Unpack/GOG_Terraform/Unpack/", ".csmesh_pc", meshPaths);
-
-        List<string> bigassMeshPaths = meshPaths.Select(path => path).Where(path => path.Contains("tharsis") || path.Contains("sky")).ToList();
-
-        // foreach (string path in bigassMeshPaths)
-        // {
-        //     meshPaths.Remove(path);
-        // }
-        
-        int numMeshes = meshPaths.Count;
-        int gridWidth = (int)Math.Sqrt(numMeshes) + 1;
-
-        int i = 0;
-        foreach (string meshPath in meshPaths)
-        {
-            MeshInstanceData meshData = LoadRfgStaticMesh(meshPath);
-
-            Mesh mesh = new Mesh(context, meshData.Vertices, meshData.Indices, meshData.Config.NumVertices, meshData.Config.NumIndices, (uint)meshData.Config.IndexSize);
-            Texture2D texture4 = LoadTextureFromFile(context, "/home/moneyl/projects/CSVulkanRenderer/CSVulkanRenderer/assets/bright_future.png");
-
-            const float meshSeparation = 2.5f;
-            int x = i % gridWidth;
-            int y = i / gridWidth;
-
-            float bigMeshSeparation = 40.0f;
-            int bigMeshIndex = bigassMeshPaths.IndexOf(meshPath);
-            bool isBigMesh = bigassMeshPaths.Contains(meshPath);
-
-            Vector3 position = isBigMesh switch
-            {
-                true => new Vector3((gridWidth + 10) * meshSeparation, 0.0f, -50.0f +  bigMeshIndex * bigMeshSeparation),
-                _ => new Vector3(x * meshSeparation, 0.0f, y * meshSeparation)
-            };
-
-            //Scene.CreateRenderObject(meshData.Config.VertexFormat.ToString(), new Vector3(-2.0f, 0.0f, 2.0f), Matrix4x4.Identity, mesh, texture4);
-            Scene.CreateRenderObject(meshData.Config.VertexFormat.ToString(), position, Matrix4x4.Identity, mesh, texture4);
-
-            i++;
-        }*/
         
         Scene.Init(new Vector2(1920, 1080));
     }
@@ -133,9 +85,22 @@ public partial class RendererTestDocumentViewModel : Document
     {
         try
         {
-            string gpuFilePath = $"{Path.GetDirectoryName(cpuFilePath)}/{Path.GetFileNameWithoutExtension(cpuFilePath)}.gsmesh_pc";
-            FileStream cpuFile = new(cpuFilePath, FileMode.Open);
-            FileStream gpuFile = new(gpuFilePath, FileMode.Open);
+            using Stream? cpuFile = PackfileVFS.OpenFile(cpuFilePath);
+            if (cpuFile == null)
+            {
+                string error = $"Failed to open cpu file for static mesh at {cpuFilePath}";
+                Log.Error(error);
+                throw new Exception(error);
+            }
+            
+            string gpuFilePath = $"/{Path.GetDirectoryName(cpuFilePath)}/{Path.GetFileNameWithoutExtension(cpuFilePath)}.gsmesh_pc";
+            using Stream? gpuFile = PackfileVFS.OpenFile(gpuFilePath);
+            if (gpuFile == null)
+            {
+                string error = $"Failed to open gpu file for static mesh at {gpuFilePath}";
+                Log.Error(error);
+                throw new Exception(error);
+            }
         
             StaticMesh staticMesh = new();
             staticMesh.ReadHeader(cpuFile);
@@ -156,42 +121,68 @@ public partial class RendererTestDocumentViewModel : Document
         }
     }
 
-    private Mesh LoadRfgStaticMeshFromFile(RenderContext context, string cpuFilePath)
+    private Mesh LoadRfgStaticMeshFromPackfile(RenderContext context, string cpuFilePath)
     {
         MeshInstanceData meshData = LoadRfgStaticMesh(cpuFilePath);
         Mesh mesh = new Mesh(context, meshData.Vertices, meshData.Indices, meshData.Config.NumVertices, meshData.Config.NumIndices, (uint)meshData.Config.IndexSize);
         return mesh;
     }
-    
-    private Texture2D LoadTextureFromFile(RenderContext context, string path)
+
+    private Texture2D LoadTextureFromPackfile(RenderContext context, string cpuFilePath, int logicalTextureIndex = 0)
     {
-        using var img = SixLabors.ImageSharp.Image.Load<SixLabors.ImageSharp.PixelFormats.Rgba32>(path);
-        ulong imageSize = (ulong)(img.Width * img.Height * img.PixelType.BitsPerPixel / 8);
-        uint mipLevels = (uint)(Math.Floor(Math.Log2(Math.Max(img.Width, img.Height))) + 1);
+        string filename = Path.GetFileName(cpuFilePath);
+        
+        string cpuFileExtension = Path.GetExtension(cpuFilePath);
+        string gpuFileExtension = cpuFileExtension switch
+        {
+            ".cpeg_pc" => ".gpeg_pc",
+            ".cvbm_pc" => ".gvbm_pc",
+            _ => throw new Exception($"Unsupported peg file extension: {cpuFileExtension}")
+        };
+        
+        using Stream? cpuFile = PackfileVFS.OpenFile(cpuFilePath);
+        if (cpuFile == null)
+        {
+            string error = $"Failed to open cpu file for texture at {cpuFilePath}";
+            Log.Error(error);
+            throw new Exception(error);
+        }
+            
+        string gpuFilePath = $"/{Path.GetDirectoryName(cpuFilePath)}/{Path.GetFileNameWithoutExtension(cpuFilePath)}{gpuFileExtension}";
+        using Stream? gpuFile = PackfileVFS.OpenFile(gpuFilePath);
+        if (gpuFile == null)
+        {
+            string error = $"Failed to open gpu file for texture at {gpuFilePath}";
+            Log.Error(error);
+            throw new Exception(error);
+        }
+        
+        PegReader reader = new();
+        LogicalTextureArchive result = reader.Read(cpuFile, gpuFile, filename, CancellationToken.None);
+        LogicalTexture logicalTexture = result.LogicalTextures[logicalTextureIndex];
+        
+        using var memoryStream = new MemoryStream();
+        logicalTexture.Data.CopyTo(memoryStream);
+        byte[] pixels = memoryStream.ToArray();
 
-        byte[] pixelData = new byte[imageSize];
-        img.CopyPixelDataTo(pixelData);
-
-        Texture2D texture = new Texture2D(context, (uint)img.Width, (uint)img.Height, mipLevels, Format.R8G8B8A8Srgb, ImageTiling.Optimal,
-                                     ImageUsageFlags.TransferSrcBit | ImageUsageFlags.TransferDstBit | ImageUsageFlags.SampledBit, MemoryPropertyFlags.DeviceLocalBit,
-                                          ImageAspectFlags.ColorBit);
-        texture.TransitionLayoutImmediate(ImageLayout.TransferDstOptimal);
-        texture.SetPixels(pixelData, true);
+        Format vulkanPixelFormat = logicalTexture.Format switch
+        {
+            RfgCpeg.Entry.BitmapFormat.PcDxt1 => Format.BC1RgbUnormBlock,
+            RfgCpeg.Entry.BitmapFormat.PcDxt3 => Format.BC2UnormBlock,
+            RfgCpeg.Entry.BitmapFormat.PcDxt5 => Format.BC3UnormBlock,
+            _ => throw new Exception($"Unsupported pixel format: {logicalTexture.Format}")
+        };
+    
+        //TODO: Need to support SRGB
+        //if (logicalTexture.Flags.HasFlag(TextureFlags.Srgb))
+        
+        Texture2D texture = new Texture2D(context, (uint)logicalTexture.Size.Width, (uint)logicalTexture.Size.Height, (uint)logicalTexture.MipLevels, vulkanPixelFormat, 
+            ImageTiling.Optimal, ImageUsageFlags.TransferSrcBit | ImageUsageFlags.TransferDstBit | ImageUsageFlags.SampledBit, MemoryPropertyFlags.DeviceLocalBit,
+            ImageAspectFlags.ColorBit);
+        texture.SetPixels(pixels, generateMipMaps: false);
         texture.CreateTextureSampler();
         texture.CreateImageView();
 
         return texture;
-    }
-    
-    private void FindFilesRecursive(string rootDirectory, string fileExtension, List<string> filePaths)
-    {
-        foreach (var file in Directory.EnumerateFiles(rootDirectory, $"*{fileExtension}"))
-        {
-            filePaths.Add(file);
-        }
-        foreach (var directory in Directory.EnumerateDirectories(rootDirectory))
-        {
-            FindFilesRecursive($"{directory}/", fileExtension, filePaths);            
-        }
     }
 }
