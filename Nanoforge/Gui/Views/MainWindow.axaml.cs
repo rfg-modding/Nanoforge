7b7d817e35a333b810e18e91ed3d3ffdbde70b0d
Luke Money <8206401+Moneyl@users.noreply.github.com>
2025-01-19 16:33:47 -0500

    Different approach to auto opening data folder dialog
    This approach still has problems, but its simpler than the previous one. Need to fix this before the first release of the C# port.

--- Nanoforge/Gui/Views/MainWindow.axaml.cs
+++ Nanoforge/Gui/Views/MainWindow.axaml.cs
@@ -8,6 +8,7 @@ using Avalonia.Interactivity;
 using Avalonia.Threading;
 using Nanoforge.Editor;
 using Nanoforge.FileSystem;
+using Nanoforge.Gui.ViewModels;
 using Nanoforge.Gui.Views.Dialogs;
 using Nanoforge.Render;
 
@@ -16,17 +17,44 @@ namespace Nanoforge.Gui.Views;
 public partial class MainWindow : Window
 {
     //TODO: Getting the current window will need to be done differently if the app starts having multiple windows
-    public static Window Instance { get; private set; } = null!;
+    public static MainWindow Instance { get; private set; } = null!;
 
     public Input Input = new();
     
-    private DispatcherTimer _timer = new(new TimeSpan(days: 0, hours: 0, minutes: 0, seconds: 0, milliseconds: 2000), DispatcherPriority.Normal, OpenDataFolderSelector);
-
+    public bool WindowLoaded = false; 
+    
     public MainWindow()
     {
         InitializeComponent();
         WindowState = WindowState.Maximized;
         Instance = this;
+        
+        //Note: Convoluted way to auto open the data folder selector. On Linux if I try to open the dialog immediately in the constructor or the Loaded event, then the main window does not maximize correctly for some reason.
+        //      Better ways of handling this are welcome! This approach might still have issues on slower machiens (longer wait could be required)
+        //      Might be better to have a separate window that opens first if the data folder isn't set. Then open the main window.
+        Dispatcher.UIThread.InvokeAsync(async () =>
+        {
+            while (!WindowLoaded)
+            {
+                await Task.Delay(50);
+            }
+
+            await Task.Delay(100);
+            if (GeneralSettings.CVar.Value.DataPath.Length == 0)
+            {
+                DataFolderSelectorDialog dataFolderSelector = new();
+                await dataFolderSelector.ShowDialog(MainWindow.Instance);
+            }
+            else
+            {
+                PackfileVFS.MountDataFolderInBackground("//data/", GeneralSettings.CVar.Value.DataPath);
+            }
+        });
+    }
+    
+    private void MainWindow_OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        WindowLoaded = true;
     }
 
     protected override void OnKeyDown(KeyEventArgs e)
@@ -45,23 +73,5 @@ public partial class MainWindow : Window
     {
         Renderer? renderer = (Application.Current as App)!.Renderer;
         renderer?.Shutdown();
-    }
-    
-    //Note: Very convoluted way to auto open the data folder selector. On Linux if I try to open the dialog immediately in the constructor or the Loaded event, then the main window does not maximize correctly for some reason.
-    //      Better ways of handling this are welcome!
-    private static async void OpenDataFolderSelector(object? sender, EventArgs args)
-    {
-        DispatcherTimer timer = (DispatcherTimer)sender!;
-        timer.Stop();
-
-        if (GeneralSettings.CVar.Value.DataPath.Length == 0)
-        {
-            DataFolderSelectorDialog dataFolderSelector = new();
-            await dataFolderSelector.ShowDialog(MainWindow.Instance); 
-        }
-        else
-        {
-            PackfileVFS.MountDataFolderInBackground("//data/", GeneralSettings.CVar.Value.DataPath);
-        }
     }
 }
